618679cbc7561942021fc27ccbab87ca9de9647a
ewigl <ewiglichtl@163.com>
2022-04-08 17:41:37 +0800

    add carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
 import { useState } from "react";
-import { Button } from 'antd';
+import { Button, Card, Carousel } from 'antd';
 import './styles/App.css';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <Button className="ant-button" onClick={() => setCount((count) => count + 1)} type="primary" >
-      count is: {count}
-    </Button>
+    <div>
+      <Carousel >
+        <div>
+          <h3 className="carousel-content">1</h3>
+        </div>
+        <div>
+          <h3 className="carousel-content">2</h3>
+        </div>
+        <div>
+          <h3 className="carousel-content">3</h3>
+        </div>
+        <div>
+          <h3 className="carousel-content">4</h3>
+        </div>
+      </Carousel>
+      {/* <Card title="Antd Card" style={{ width: 300 }}>
+        <Button onClick={() => setCount((count) => count + 1)} type="primary" >
+          count is: {count}
+        </Button>
+      </Card> */}
+    </div>
   );
 }
 
